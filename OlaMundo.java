a989d9937a7dbb1897de70e45883844a7c95b4ad
m-godoi <matheus.sgalves@gmail.com>
2025-09-01 08:57:20 -0300

    add mensagem

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,5 +1,5 @@
-class OlaMundo {
-	public static void main(String[] args){
-		System.out.println("Olá, mundo!");
-	}
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+    }
 }
